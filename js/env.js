62a97733c4f0c06c2f26026df58ccb687dc8ce3c
420230316-crypto <420230316@fa-hists.edu.eg>
2026-04-13 01:26:55 +0200

    Add files via upload

---
+++ js/env.js
@@ -0,0 +1,5 @@
+const ENV = {
+    SUPABASE_URL: 'https://tcvyfaxdvveniwsgnbxk.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_O_13Ds6U69DTdSMikiFa4w_sA3oSBwS',
+    BREVO_KEY: 'xkeysib-da3379308edc39f955599a4fbad9cd1ea2513f8ec26f8abec46675ae565011d4-'
+};
