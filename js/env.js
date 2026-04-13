228bf10af54ff85b90ffad61c7846a5c4e41ab0f
420230316-crypto <420230316@fa-hists.edu.eg>
2026-04-13 02:44:40 +0200

    Delete js directory

--- js/env.js
+++
@@ -1,5 +0,0 @@
-const ENV = {
-    SUPABASE_URL: 'https://tcvyfaxdvveniwsgnbxk.supabase.co',
-    SUPABASE_KEY: 'sb_publishable_O_13Ds6U69DTdSMikiFa4w_sA3oSBwS',
-    BREVO_KEY: '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'
-};
